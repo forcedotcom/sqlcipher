c586057dd031f5b29684c20abdb5b1b61a296928
Kevin Hawkins <khawkins@salesforce.com>
2016-03-22 10:42:55 -0700

    Changes to create a bitcode-for-archive library

--- build_for_sdk.sh
+++ build_for_sdk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xcodebuild ARCHS="armv7 armv7s arm64" ONLY_ACTIVE_ARCH=NO -configuration Release -project sqlcipher.xcodeproj
-xcodebuild ARCHS="x86_64 i386" ONLY_ACTIVE_ARCH=NO -configuration Release -project sqlcipher.xcodeproj -sdk iphonesimulator
-lipo -create build/Release-iphoneos/libsqlcipher.a build/Release-iphonesimulator/libsqlcipher.a -output libsqlcipher.a
+xcodebuild ARCHS="armv7 armv7s arm64" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphoneos -configuration Release -project sqlcipher.xcodeproj -target sqlcipher clean install
+xcodebuild ARCHS="x86_64 i386" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator -configuration Release -sdk iphonesimulator -project sqlcipher.xcodeproj -target sqlcipher clean install
+lipo -create build/UninstalledProducts/iphoneos/libsqlcipher.a build/UninstalledProducts/iphonesimulator/libsqlcipher.a -output libsqlcipher.a
