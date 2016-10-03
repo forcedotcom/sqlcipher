de24e342669c6d30f9c73144b37f806e488238dd
Bharath Hariharan <bhariharan@salesforce.com>
2016-10-03 15:22:23 -0700

    Updating script to use bit code

--- build_for_sdk.sh
+++ build_for_sdk.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-xcodebuild ARCHS="armv7 armv7s arm64" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphoneos -configuration Release -project sqlcipher.xcodeproj -target sqlcipher clean install
+xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ARCHS="armv7 armv7s arm64" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphoneos -configuration Release -project sqlcipher.xcodeproj -target sqlcipher clean install
 if [ -f "build/UninstalledProducts/libsqlcipher.a" ]; then
 	mkdir -p build/UninstalledProducts/iphoneos
 	mv build/UninstalledProducts/libsqlcipher.a build/UninstalledProducts/iphoneos/libsqlcipher.a
 fi
-xcodebuild ARCHS="x86_64 i386" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator -configuration Release -sdk iphonesimulator -project sqlcipher.xcodeproj -target sqlcipher clean install
+xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ARCHS="x86_64 i386" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator -configuration Release -sdk iphonesimulator -project sqlcipher.xcodeproj -target sqlcipher clean install
 if [ -f "build/UninstalledProducts/libsqlcipher.a" ]; then
 	mkdir -p build/UninstalledProducts/iphonesimulator
 	mv build/UninstalledProducts/libsqlcipher.a build/UninstalledProducts/iphonesimulator/libsqlcipher.a
