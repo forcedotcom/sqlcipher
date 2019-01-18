3a0b530c8b5a7bc5e239be90c8dd07b1a0d4835b
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-01-17 18:05:05 -0800

    Updated buid_for_sdk.sh

--- build_for_sdk.sh
+++ build_for_sdk.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ARCHS="armv7 armv7s arm64" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphoneos -configuration Release -project sqlcipher.xcodeproj -target sqlcipher clean install
-if [ -f "build/UninstalledProducts/libsqlcipher.a" ]; then
-	mkdir -p build/UninstalledProducts/iphoneos
-	mv build/UninstalledProducts/libsqlcipher.a build/UninstalledProducts/iphoneos/libsqlcipher.a
+if [ -f "build/UninstalledProducts/iphoneos/libsqlcipher.a" ]; then
+	mv build/UninstalledProducts/iphoneos/libsqlcipher.a libsqlcipher-iphoneos.a
 fi
 xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ARCHS="x86_64 i386" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator -configuration Release -sdk iphonesimulator -project sqlcipher.xcodeproj -target sqlcipher clean install
-if [ -f "build/UninstalledProducts/libsqlcipher.a" ]; then
-	mkdir -p build/UninstalledProducts/iphonesimulator
-	mv build/UninstalledProducts/libsqlcipher.a build/UninstalledProducts/iphonesimulator/libsqlcipher.a
+if [ -f "./build/UninstalledProducts/iphonesimulator/libsqlcipher.a" ]; then
+	mv ./build/UninstalledProducts/iphonesimulator/libsqlcipher.a libsqlcipher-iphonesimulator.a
 fi
-lipo -create build/UninstalledProducts/iphoneos/libsqlcipher.a build/UninstalledProducts/iphonesimulator/libsqlcipher.a -output libsqlcipher.a
+lipo -create libsqlcipher-iphoneos.a libsqlcipher-iphonesimulator.a -output libsqlcipher.a
