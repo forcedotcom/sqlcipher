d44e61982e9f53f34a345c1398e993255ad01c28
Brianna Birman <brianna.birman@salesforce.com>
2022-05-26 19:08:15 -0700

    Fix building for simulators on m1

--- build_for_sdk.sh
+++ build_for_sdk.sh
@@ -3,7 +3,7 @@ xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-b
 if [ -f "build/UninstalledProducts/iphoneos/libsqlcipher.a" ]; then
 	mv build/UninstalledProducts/iphoneos/libsqlcipher.a libsqlcipher-iphoneos.a
 fi
-xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator -configuration Release -sdk iphonesimulator -project sqlcipher.xcodeproj -target sqlcipher clean install
+xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ONLY_ACTIVE_ARCH=NO -destination "generic/platform=iOS Simulator" -configuration Release -sdk iphonesimulator -project sqlcipher.xcodeproj -target sqlcipher clean install
 if [ -f "./build/UninstalledProducts/iphonesimulator/libsqlcipher.a" ]; then
 	mv ./build/UninstalledProducts/iphonesimulator/libsqlcipher.a libsqlcipher-iphonesimulator.a
 fi
